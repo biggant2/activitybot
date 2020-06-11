fa055ec514179775970e5251433c0fb4e6f057dd
biggant2 <cwolf1288@gmail.com>
2020-06-11 11:31:11 -0700

    before and after

--- commands/getmessages.js
+++ commands/getmessages.js
@@ -3,8 +3,8 @@ const fetchMessages = require('../fetchMessages.js');
 
 exports.run = async (client, message, args) => {
     if(!+args[0]) return;
-    fetchMessages.fetch(message.channel, +args[0])
-        .then(messages => message.channel.send(messages.array().length))
+    fetchMessages.fetch(message.channel, {num: +args[0], before: args[1], after: args[2]})
+        .then(messages => message.channel.send(messages.array().map(message => message.content)))
 }
 
 exports.help = {
--- fetchMessages.js
+++ fetchMessages.js
@@ -1,21 +1,18 @@
 const Discord = require('discord.js');
 
-exports.fetch = async (channel, num) => {
+exports.fetch = async (channel, {num = 100, before, after}) => {
     let messages = new Discord.Collection();
     let timesToRepeat = Math.floor(num / 100);
-    if(timesToRepeat === 0) {
-        messages = await channel.fetchMessages({limit: num});
-    } else {
-        let lastId = channel.lastMessageID;
-        for(let i = 0; i < timesToRepeat; i++) {
-            let partialMessages = await channel.fetchMessages({limit: 100, before: lastId})
-            messages = messages.concat(partialMessages)
-            lastId = partialMessages.last().id;
-        }
-        if(num % 100 !== 0) {
-            let remainingMessages = await channel.fetchMessages({limit: num % 100, before: lastId});
-            messages = messages.concat(remainingMessages)
-        }
+    let lastId = before || channel.lastMessageID;
+    
+    for(let i = 0; i < timesToRepeat; i++) {
+        let partialMessages = await channel.fetchMessages({limit: 100, before: lastId, after: after})
+        messages = messages.concat(partialMessages)
+        lastId = partialMessages.last().id;
+    }
+    if(num % 100 !== 0) {
+        let remainingMessages = await channel.fetchMessages({limit: num % 100, before: lastId, after: after});
+        messages = messages.concat(remainingMessages)
     }
 
     return messages;
